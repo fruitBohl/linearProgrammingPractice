e823b88eff9a25659a164f3a33d11c0bacc30e7e
Harrison Maxwell Bohl <74139799+fruitBohl@users.noreply.github.com>
2021-09-16 19:22:33 +1000

    First time messing around with optimization of system of linear equations.

---
+++ src/firstMessAround.py
@@ -0,0 +1,24 @@
+import pulp
+from scipy.optimize import linprog
+import numpy
+
+
+def optimization():
+    # put system of equations into arrays
+    obj = [-1, -2]
+
+    lhsIneq = [[2, 1], [-4, 5], [1, -2]]
+    rhsIneq = [20, 10, 2]
+    lhsEq = [[-1, 5]]
+    rhsEq = 15
+
+    # define bounds for each variable
+    bounds = [(0, numpy.inf), (0, numpy.inf)]
+
+    # optimize and output system
+    opt = linprog(obj, lhsIneq, rhsIneq, lhsEq, rhsEq, bounds, "revised simplex")
+    print(opt)
+
+
+if __name__ == "__main__":
+    optimization()
